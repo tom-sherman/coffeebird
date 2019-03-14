80e890d1c5d53955e2982ac8e875477e7af03748
Tom Sherman <the.tomsherman@gmail.com>
2019-03-14 12:17:53 +0000

    Export rule and conditions

--- src/parser/index.js
+++ src/parser/index.js
@@ -2,5 +2,7 @@ module.exports = {
   Concept: require('./concept').Concept,
   Rel: require('./relationship').Rel,
   Instance: require('./instance').Instance,
-  Fact: require('./fact').Fact
+  Fact: require('./fact').Fact,
+  Rule: require('./rule').Rule,
+  Condition: require('./condition').Condition
 }
