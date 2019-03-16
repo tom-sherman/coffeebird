e5bc2bbfc498597fa28f01a79b523e339be1bdcf
Tom Sherman <the.tomsherman@gmail.com>
2019-03-16 12:52:08 +0000

    Raname to coffeebird

--- test/coffeebird.spec.js
+++ test/coffeebird.spec.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const { transpile } = require('../src/probird')
+const { transpile } = require('../src/coffeebird')
 
 it('transform', function() {
   const transformed = transpile(`
