57938ec0d2178150cde92d7f643960003f2f8eb2
Tom Sherman <the.tomsherman@gmail.com>
2019-03-20 12:00:49 +0000

    Add basic CLI

---
+++ src/cli.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const { parse, transpile } = require('./coffeebird')
+
+const argv = require('yargs')
+  .command(['transpile [input]', '*'], 'transpile a file to RBLang', yargs => {
+    yargs.positional('input', {
+      describe: 'The file to transpile.'
+    })
+  })
+  .option('output', {
+    alias: 'o'
+  })
+  .argv
+
+const file = fs.readFileSync(argv.input, { encoding: 'utf8' })
+
+const transpiled = transpile(file)
+
+if (argv.output) {
+  fs.writeFileSync(argv.output, transpiled)
+} else {
+  console.log(transpiled)
+}
